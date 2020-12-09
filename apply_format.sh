4ea03db4e6f26ffe3f0b5d0475792abbadccce9c
Wolfgang Merkt <w.merkt@gmail.com>
2020-12-09 22:25:13 +0000

    Missing formatting

--- apply_format.sh
+++ apply_format.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh
-find -name '*.cpp' -o -name '*.hpp' | xargs clang-format-3.9 -style=file -i
-# skip -o -name '*.h' to avoid PCL header being formatted
-
+find -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs clang-format-3.9 -style=file -i
